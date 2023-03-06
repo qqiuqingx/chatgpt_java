2f8ad0652efb686a78b07d380efe0151f9cd8553
qqiuqingx <48545125+qqiuqingx@users.noreply.github.com>
2023-03-06 17:07:46 +0800

    Update ThymeleafControlle.java

--- src/main/java/com/example/javachatbot/controller/ThymeleafControlle.java
+++ src/main/java/com/example/javachatbot/controller/ThymeleafControlle.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ThymeleafControlle {
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/")
     public String index(Model model) {
         return "index";
     }
